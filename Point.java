3cfd42feb173795176ae37d19081cf830417e8a7
David Chong <djchong24@gmail.com>
2020-10-29 22:58:36 -0400

    finished equals method

--- Point.java
+++ Point.java
@@ -29,12 +29,19 @@ public class Point{
         return(result);
       }
 
+    public boolean equals(Point other) {
+        return(x == other.x && y == other.y);
+    }
+
     public static void main(String[] args) {
         Point point1 = new Point(3.0, 0.0);
         Point point2 = new Point(0.0, 0.0);
         Point point3 = new Point(0.0, 4.0);
+        Point point4 = new Point(3.0, 0.0);
         System.out.println(point1.distanceTo(point2));
         System.out.println(point2.distanceTo(point3));
         System.out.println(point1.distanceTo(point3));
+        System.out.println(point1.equals(point4));
+        System.out.println(point1.equals(point2));
     }
 }
